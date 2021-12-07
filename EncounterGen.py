9fa2fffa6c354a858286ce1ddf029ab102904b8c
Studio-13-Productions <Studio13productions@outlook.com>
2021-12-06 19:48:21 -0500

    imported random, made random index bp code 4 loop

--- EncounterGen.py
+++ EncounterGen.py
@@ -1,19 +1,27 @@
-
+import random
 
 class EncounterCard:
     def __init__(self):
         self.userInput = ""
     
-class NPC:
+class npcCard:
     def __init__(self):
         self.name = "Rylea"
         self.pospWorldLocations  = ['1','2','3']
         self.pospMapLocaions = ['1','2','3']
-        self.pref = ['Fitness, Observation']
-        self.dislikes['Research', 'Resolve']
+        self.goodPref = ['Fitness', 'Observation']
+        self.badPref = ['Fitness', 'Observation']
         self.gossipText = ['Did you know I\'m an NPC?']
         
 
-Rylea = NPC()
+#def GenerateNpcCard(npcCard):
+    #npcCard= ''
 
-print(Rylea.name)
+ryleaCard = npcCard()
+
+
+#for x, trait in ryleaCard.__dict__.items():
+
+ #   print(trait)
+print(ryleaCard.goodPref[(random.randint(1, 2)-1)])
+print 
